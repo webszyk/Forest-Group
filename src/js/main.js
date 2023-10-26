7c6c511a61e80085a1b7e1a6814da2a9d8aca065
KarlaSz <128708615+KarlaSz@users.noreply.github.com>
2023-10-26 15:29:46 +0200

    contact page and menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,14 @@
+//nav mobile/desktop
 const navBtn = document.querySelector(".hamburger");
 const navView = document.querySelector(".nav__mobile");
 const nav = document.querySelector(".nav");
+
+//footer year
 const footerYear = document.querySelector(".footer__year");
+
+/* //scrolsspy nac
+const menuItems = document.querySelectorAll(".nav__items");
+const scrollSpySpection = document.querySelectorAll(".section"); */
 
 const showMobileNav = () => {
   navBtn.classList.toggle("is-active");
@@ -19,6 +26,33 @@ const navShadow = () => {
     nav.classList.add("shadow-bg");
   } else {
     nav.classList.remove("shadow-bg");
+  }
+};
+
+const handleScrollSpy = () => {
+  if (document.body.classList.contains("main-page")) {
+    const sections = [];
+
+    scrollSpySpection.forEach((section) => {
+      /* wartosc scrolla o jaki przesunelismy */
+      /*    console.log(window.scrollY); */
+
+      /* odleglosc od gornej krawedzi przegladarki */
+      /*  console.log(section.offsetTop); */
+
+      /* wysokosc kazdej skecji */
+      /*   console.log(section.offsetHeight); */
+
+      if (window.scrollY <= section.offsetTop + section.offsetHeight - 88) {
+        sections.push(section);
+        console.log(sections);
+
+        const activeSection = document.querySelector(`a[href="index.html#"]`);
+
+        menuItems.forEach((item) => item.classList.remove("active"));
+        activeSection.classList.add("active");
+      }
+    });
   }
 };
 
